b9fdaac1b64ec7ae29b5a08e7cbe0502d1321c6f
Tymon Woźniak <tymonek12345@gmail.com>
2024-04-24 10:12:43 +0200

    Przygotowanie api pod zmiane biografii

---
+++ src/utils/UserUpdateBio.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+import { API_V1 } from '@/lib/api/api';
+
+const API_URL_USER_UPDATE_BIO = `${API_V1}/user/update/bio`;
+
+const UserUpdateBio = (token: string, bio: string) => {
+  const url = API_URL_USER_UPDATE_BIO;
+  const res = axios.post(url);
+};
+
+export { UserUpdateBio };
